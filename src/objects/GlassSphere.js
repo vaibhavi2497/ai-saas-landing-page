ee746239112f5fe58e3c2af66bc8d842d15ee96a
Vaibhavi <vaibhavinalawade2018@gmail.com>
2026-07-12 15:05:22 +0530

    Fix Render deployment

--- src/objects/GlassSphere.js
+++ src/objects/GlassSphere.js
@@ -2,17 +2,7 @@ import * as THREE from "three";
 
 const geometry = new THREE.SphereGeometry(1, 64, 64);
 
-const material = new THREE.MeshPhysicalMaterial({
-    color: 0xffffff,
-    transmission: 1,
-    transparent: true,
-    roughness: 0,
-    metalness: 0,
-    thickness: 1.5,
-    ior: 1.5,
-    clearcoat: 1,
-    clearcoatRoughness: 0
-});
+const material = new THREE.MeshNormalMaterial();
 
 const sphere = new THREE.Mesh(geometry, material);
 

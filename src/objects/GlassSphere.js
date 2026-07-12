124a0b71f5c48bfd7dc1dfee65efab6676be7123
Vaibhavi <vaibhavinalawade2018@gmail.com>
2026-07-12 15:08:12 +0530

    fix sphere bug

--- src/objects/GlassSphere.js
+++ src/objects/GlassSphere.js
@@ -2,7 +2,23 @@ import * as THREE from "three";
 
 const geometry = new THREE.SphereGeometry(1, 64, 64);
 
-const material = new THREE.MeshNormalMaterial();
+const material = new THREE.MeshPhysicalMaterial({
+    color: 0xffffff,
+    transmission: 0.9,
+    opacity: 1,
+    transparent: true,
+
+    roughness: 0.1,
+    metalness: 0,
+
+    thickness: 2,
+    ior: 1.5,
+
+    clearcoat: 1,
+    clearcoatRoughness: 0,
+
+    envMapIntensity: 2
+});
 
 const sphere = new THREE.Mesh(geometry, material);
 

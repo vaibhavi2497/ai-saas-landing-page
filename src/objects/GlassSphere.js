5364d475702d13f2b6d1831e96432deb26faaf90
vaibhavi2497 <vaibhavinalawade2018@gmail.com>
2026-07-13 10:52:34 +0530

    change in glassSphere

--- src/objects/GlassSphere.js
+++ src/objects/GlassSphere.js
@@ -3,23 +3,16 @@ import * as THREE from "three";
 const geometry = new THREE.SphereGeometry(1, 64, 64);
 
 const material = new THREE.MeshPhysicalMaterial({
-    color: 0xffffff,
-
-    transmission: 1,
-    transparent: true,
-    opacity: 1,
-
-    roughness: 0,
-    metalness: 0,
-
-    thickness: 5,
-    ior: 1.52,
-
-    clearcoat: 1,
-    clearcoatRoughness: 0,
-
-    envMapIntensity: 2
+     color: 0xffffff,
+    roughness: 0.05,           // Slightly above 0 adds realistic micro-surface glare
+    transmission: 1.0,         // Pure transmissive glass
+    thickness: 1.5,            // Bends light through the sphere volume
+    ior: 1.5,                  // Standard index of refraction for glass
+    clearcoat: 1.0,            // Extra outer reflective layer
+    clearcoatRoughness: 0.0,
+    envMapIntensity: 1.5       // Boosts the HDR reflections on the glass surface
 });
+
 
 const sphere = new THREE.Mesh(geometry, material);
 
